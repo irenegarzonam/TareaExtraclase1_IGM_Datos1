f97ae16169f5bbbde96b92c529131317a1d2fae6
Irene <igarzona@estudiantec.cr>
2021-03-24 16:26:29 -0600

    TryCatchCheckedException

---
+++ src/cr/ac/itcr/UncheckedException.java
@@ -0,0 +1,35 @@
+package cr.ac.itcr;
+
+//Bibliotecas
+import java.util.Scanner;
+
+public class UncheckedException
+{
+    public static void main(String args[])
+    {
+        //crea objeto scanner para obtener ventana de comandos
+        Scanner entrada = new Scanner( System.in );
+
+        int numero1;
+        int numero2;
+        int cociente;
+
+        System.out.print( "Escriba el primer numero entero: " ) ;
+        numero1 = entrada.nextInt();
+
+        System.out.print( "Escriba el segundo numero entero: " );
+        numero2 = entrada. nextInt();
+
+        try
+        {
+            cociente = numero1/numero2;
+        }catch (ArithmeticException ex)
+        {
+            System.err.println(ex);
+            System.err.println("Es imposible dividir entre 0");
+        }finally
+        {
+            System.out.printf("Division entre %d y %d", numero1, numero2);
+        }
+    }
+}
